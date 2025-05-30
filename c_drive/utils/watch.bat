ece26f0d398a077dcfe19c35df99754deebba195
Tom Szwaja <tsxoxo@proton.me>
2025-05-30 17:38:54 +0200

    Try to improve dev experience: set up asm LSP; dosbox config; watch script

--- c_drive/utils/watch.bat
+++ c_drive/utils/watch.bat
@@ -4,16 +4,17 @@ REM Prog to make fasm compile/run cycle a little faster.
 REM Assumes you are waiting for keypress to exit in your program.
 
 :LOOP
+  REM Compile and run
+  fasm %1%.asm
+  if errorlevel 1 goto EXIT 
+  %1%
+
   REM give us a chance to quit
   REM Remove if you want blazingly fast,
   REM but have to force quite dosbox to exit.
   CHOICE /Cnq /T:n,01 /N Press Q to quit
   if errorlevel 2 goto EXIT
 
-  REM Compile and run
-  fasm %1%.asm
-  if errorlevel 1 goto EXIT 
-  %1%
   GOTO LOOP
 
 :EXIT
